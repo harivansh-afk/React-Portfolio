6d173756859b5d5658c40587ca9a88080d3ad993
Harivansh Rathi <rathiharivansh@gmail.com>
2025-01-03 17:42:08 +0530

    chore: dark mode default

--- src/components/themetoggle/index.js
+++ src/components/themetoggle/index.js
@@ -3,13 +3,13 @@ import { WiMoonAltWaningCrescent4 } from "react-icons/wi";
 
 
 const Themetoggle = () => {
-  const [theme, settheme] = useState(localStorage.getItem("theme"));
+  const [theme, settheme] = useState(localStorage.getItem("theme") || "dark");
   const themetoggle = () => {
     settheme(theme === "dark" ? "light" : "dark");
   };
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme ); 
+    localStorage.setItem('theme', theme );
   }, [theme]);
   return (
     <div className="nav_ac" onClick={themetoggle}>
